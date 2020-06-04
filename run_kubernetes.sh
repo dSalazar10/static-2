a0ef229b6456dd92a40838c2aeeaef0745a7ca54
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2020-06-04 15:04:20 -0700

    Update run_kubernetes.sh
    Update the template to include the necessary source code to pull from Docker Hub and deploy into a local cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+DOCKERPATH="dsalazar10/udacity"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app\
+    --generator=run-pod/v1\
+    --image=$DOCKERPATH\
+    --port=80 --labels app=app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward app 80:80
